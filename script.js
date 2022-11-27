c9967b797cdbc60f025e9ff002e1eaf89d18eba4
rgholland90 <rgholland90@gmail.com>
2022-11-27 00:40:10 -0500

    Add playRound function to compare choices and declare round winner

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ function getComputerChoice() {
   return choices[randomNum];
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection[0].toUpperCase() +
+    playerSelection.substring(1).toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return `Tie. Both players selected ${playerSelection}.`;
+  }
+
+  if (playerSelection === "Rock") {
+    return computerSelection === "Paper"
+      ? `You lose. Computer's ${computerSelection} beat your ${playerSelection}.`
+      : `You win. Your ${playerSelection} beat computer's ${computerSelection}.`;
+  }
+
+  if (playerSelection === "Paper") {
+    return computerSelection === "Scissors"
+      ? `You lose. Computer's ${computerSelection} beat your ${playerSelection}.`
+      : `You win. Your ${playerSelection} beat computer's ${computerSelection}.`;
+  }
+
+  if (playerSelection === "Scissors") {
+    return computerSelection === "Rock"
+      ? `You lose. Computer's ${computerSelection} beat your ${playerSelection}.`
+      : `You win. Your ${playerSelection} beat computer's ${computerSelection}.`;
+  }
+}
+
+console.log(playRound("ROCK", "Scissors"));
